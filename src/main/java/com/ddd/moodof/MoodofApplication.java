6379c50c30c878f28f118718ab0b480136fffe38
gwanhyeon <kgh940525@gmail.com>
2021-04-05 15:57:22 +0900

    Init project

---
+++ src/main/java/com/ddd/moodof/MoodofApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.moodof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodofApplication.class, args);
+	}
+
+}
