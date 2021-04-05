ab34854a581ad04ba783eb9acf3287ec836e4d48
gwanhyeon <kgh940525@gmail.com>
2021-04-05 16:34:12 +0900

    chore: apply project rules

---
+++ src/main/java/com/ddd/moodof/domain/entity/User.java
@@ -0,0 +1,26 @@
+package com.ddd.moodof.domain.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String nickname;
+    private String profileUrl;
+    private LocalDateTime createdTime;
+    private LocalDateTime modifiedTime;
+}
