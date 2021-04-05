e765b49250485a55239836945abb01ecaad85bcd
gwanhyeon <kgh940525@gmail.com>
2021-04-05 15:58:23 +0900

    Init project (#2)

---
+++ src/test/java/com/ddd/moodof/MoodofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddd.moodof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
