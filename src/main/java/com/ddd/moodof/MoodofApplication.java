c14d1d8fa711a9064ee5a85bb5e7e4cb0a077b2e
Gyeongjun Kim <lastpuzly@gmail.com>
2021-04-15 01:59:15 +0900

    feat: add swagger

--- src/main/java/com/ddd/moodof/MoodofApplication.java
+++ src/main/java/com/ddd/moodof/MoodofApplication.java
@@ -1,21 +1,13 @@
 package com.ddd.moodof;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class MoodofApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoodofApplication.class, args);
-	}
-	@Bean
-	JPAQueryFactory jpaQueryFactory(EntityManager em){
-		return new JPAQueryFactory(em);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoodofApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/ddd/moodof/adapter/infrastructure/configuration/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.ddd.moodof.adapter.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage("com.ddd.moodof"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
