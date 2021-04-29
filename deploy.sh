129ed8d271169f25fee53372ee01915fb305d0ff
Gyeongjun Kim <lastpuzly@gmail.com>
2021-04-30 00:29:17 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 APP_NAME=moodof-server
-JAR_NAME=$(ls | grep '.jar' | tail -n 1)
-
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할것 없음."
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  ehco "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_NAME 배포"
-nohup java -jar $JAR_NAME &
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls | grep '.jar' | tail -n 1)
+
+echo "> JAR name: JAR_NAME"
+echo "> $JAR_NAME에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $JAR_NAME > ./nohup.out 2>&1 & 
