616dca9e022dbb7f4c00d03db00aa9a6d7c83560
Gyeongjun Kim <lastpuzly@gmail.com>
2021-05-28 21:43:00 +0900

    fix: board 생성시 userId 검증 수정

--- src/main/java/com/ddd/moodof/domain/model/board/BoardRepository.java
+++ src/main/java/com/ddd/moodof/domain/model/board/BoardRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Optional<Board> findByPreviousBoardIdAndIdNot(Long previousBoardId, Long id);
-
     Optional<Board> findByPreviousBoardId(Long previousBoardId);
 
     Optional<Board> findByPreviousBoardIdAndCategoryId(Long previousBoardId, Long categoryId);
@@ -14,4 +12,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     void deleteAllByCategoryId(Long categoryId);
 
     boolean existsByIdAndUserId(Long id, Long userId);
+
+    Optional<Board> findByUserIdAndPreviousBoardIdAndIdNot(Long userId, Long previousBoardId, Long id);
 }
